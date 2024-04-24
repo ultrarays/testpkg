54aa51680f77b77fca3eab098b283de507ebf751
ultrarays <xiaobo@192.168.1.6>
2024-04-25 05:33:44 +0800

    feat: 新增加 hello 库

---
+++ hello/hello.go
@@ -0,0 +1,7 @@
+package hello
+
+import "fmt"
+
+func SayHello2Someboy(name string) {
+	fmt.Printf("%s 你好\n", name)
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/ultrarays/testpkg/hello"
+
+func main() {
+	hello.SayHello2Someboy("zhangsan")
+}
